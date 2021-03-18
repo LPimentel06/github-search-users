95e05fbecddb48671f1fa8ddb1e96e697ddf2bd1
Luís Pimentel <luis.pimentel@bee-eng.pt>
2021-03-18 18:58:37 +0000

    Persisting authentication through pages with localStorage

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain={process.env.REACT_APP_AUTH_DOMAIN} clientId={process.env.REACT_APP_AUTH_CLIENT_ID} redirectUri={window.location.origin}>
+    <Auth0Provider domain={process.env.REACT_APP_AUTH_DOMAIN} clientId={process.env.REACT_APP_AUTH_CLIENT_ID} redirectUri={window.location.origin} cacheLocation="localstorage">
       <GithubProvider>
         <App />
       </GithubProvider>
